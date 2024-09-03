6ccd19d8c9cbeaca32da88e6742c708bfff10d68
br4d112 <dalejandro011207@hotmail.com>
2024-09-02 20:37:43 -0500

    cambios en Run

--- src/Run.java
+++ src/Run.java
@@ -6,7 +6,6 @@ import models.Candidato;
 public class Run {
     public static void main(String[] args) {
         String archivo = "candidatos.txt";
-        new IOMngr();
         List<Candidato> candidatos = IOMngr.leerCandidatosDesdeArchivo(archivo);
         int a = 1;
         for (Candidato candidato : candidatos) {
